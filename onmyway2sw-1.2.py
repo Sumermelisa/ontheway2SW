b0c26f6e42dc5316662e360a1ac261510a607ca4
Sümer Melisa Başer <100526755+Sumermelisa@users.noreply.github.com>
2022-06-24 15:21:42 +0300

    Add files via upload

---
+++ onmyway2sw-1.2.py
@@ -0,0 +1,6 @@
+import random
+
+answers=["it is certain", "outlook good", "you may rely on it", "ask again later",
+         "concentrate and ask again", "reply hazy, try again", "my reply is no", "my sources say no"]
+input("Ask your question:")
+print("answer:",random.choice(answers))
